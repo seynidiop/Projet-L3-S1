82d641f033ccf86a85199c8c8559a191c3c8bf3c
seyni diop <seynidiop071@gmail.com>
2025-12-30 04:31:59 +0000

    Modification Statut et Annulation Commande

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -11,6 +11,7 @@ use App\Repository\ClientRepository;
 use App\Repository\CommandeRepository;
 use App\DTO\CommandeDTO;
 use App\DTO\CommandeSearchDTO;
+use App\Entity\Commande;
 
 class CommandeController extends AbstractController
 {
@@ -21,73 +22,107 @@ class CommandeController extends AbstractController
 
     private const limit=3;
 
-    #[Route('/commande/list', name: 'app_commande_list')]
-    public function list(Request $request): Response
-    {   
-        $filtre = [];
-         $searchFormDTO = new CommandeSearchDTO();
-        $form = $this->createForm(\App\Form\CommandeSearchType::class, $searchFormDTO,[
-            'method' => 'GET',
-            'csrf_protection' => false,
-        ]);
-        $form->handleRequest($request);
-         $page=$request->query->get("page",1);
-        $offset=($page-1)*self::limit;
-        $commandes = $this->commandeRepository->findBy($filtre,["id"=>"DESC"],self::limit,$offset);
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($searchFormDTO->phone !== null) {
-               $commandes = array_filter($commandes, function ($commande) use ($searchFormDTO) {
-                return $commande->getClient()
-                    && str_contains(
-                        $commande->getClient()->getPhone(),
-                        $searchFormDTO->phone
-                    );
-            });
-            }
-            if ($searchFormDTO->produitType !== null) {
-                switch ($searchFormDTO->produitType) {
-                    case 'burger':
-                        $filtre['menu'] = $searchFormDTO->menu;
-                        break;
-                    case 'menu':
-                        $filtre['burger'] = $searchFormDTO->burger;
-                        break;
-                    
-                    default:
-                       
-                        break;
+        #[Route('/commande/list', name: 'app_commande_list')]
+        public function list(Request $request): Response
+        {   
+            $filtre = [];
+            $searchFormDTO = new CommandeSearchDTO();
+            $form = $this->createForm(\App\Form\CommandeSearchType::class, $searchFormDTO,[
+                'method' => 'GET',
+                'csrf_protection' => false,
+            ]);
+            $form->handleRequest($request);
+            $page=$request->query->get("page",1);
+            $offset=($page-1)*self::limit;
+            $commandes = $this->commandeRepository->findBy($filtre,["id"=>"DESC"],self::limit,$offset);
+            if ($form->isSubmitted() && $form->isValid()) {
+                if ($searchFormDTO->phone !== null) {
+                $commandes = array_filter($commandes, function ($commande) use ($searchFormDTO) {
+                    return $commande->getClient()
+                        && str_contains(
+                            $commande->getClient()->getPhone(),
+                            $searchFormDTO->phone
+                        );
+                });
                 }
-            }
-            if ($searchFormDTO->datecommande) {
-                $filtre['datecommande'] = $searchFormDTO->datecommande;
-            }
-            var_dump($filtre);
-       
-        
-        $commandesDTO= CommandeDTO::toEntityArray($commandes);
-        $count= $this->commandeRepository->count();
-        $nbrePages=ceil($count/self::limit);
+                if ($searchFormDTO->produitType !== null) {
+                    switch ($searchFormDTO->produitType) {
+                        case 'burger':
+                            $filtre['menu'] = $searchFormDTO->menu;
+                            break;
+                        case 'menu':
+                            $filtre['burger'] = $searchFormDTO->burger;
+                            break;
+                        
+                        default:
+                        
+                            break;
+                    }
+                }
+                if ($searchFormDTO->datecommande) {
+                    $filtre['datecommande'] = $searchFormDTO->datecommande;
+                }
+            
+            $commandesDTO= CommandeDTO::toEntityArray($commandes);
+            $count= $this->commandeRepository->count();
+            $nbrePages=ceil($count/self::limit);
 
-        return $this->render('commande/list.html.twig', [
-            'commandes' => $commandesDTO,
-            'totalPages'=>$nbrePages,
-            'currentPage'=>$page,
-            'formSearch' => $form->createView(),
-        ]);
+            return $this->render('commande/list.html.twig', [
+                'commandes' => $commandesDTO,
+                'totalPages'=>$nbrePages,
+                'currentPage'=>$page,
+                'formSearch' => $form->createView(),
+            ]);
+        }
+
+        $page=$request->query->get("page",1);
+            $offset=($page-1)*self::limit;
+            $commandes = $this->commandeRepository->findBy($filtre,["id"=>"DESC"],self::limit,$offset);
+            $commandesDTO= CommandeDTO::toEntityArray($commandes);
+            $count= $this->commandeRepository->count();
+            $nbrePages=ceil($count/self::limit);
+
+            return $this->render('commande/list.html.twig', [
+                'commandes' => $commandesDTO,
+                'totalPages'=>$nbrePages,
+                'currentPage'=>$page,
+                'formSearch' => $form->createView(),
+            ]);
     }
 
-     $page=$request->query->get("page",1);
-        $offset=($page-1)*self::limit;
-        $commandes = $this->commandeRepository->findBy($filtre,["id"=>"DESC"],self::limit,$offset);
-        $commandesDTO= CommandeDTO::toEntityArray($commandes);
-        $count= $this->commandeRepository->count();
-        $nbrePages=ceil($count/self::limit);
+    #[Route('/commande/{id}/terminer', name: 'app_commande_terminer', methods: ['GET'])]
+    public function terminerCommande(int $id, EntityManagerInterface $em): Response {
+        $commande = $em->getRepository(Commande::class)->find($id);
 
-        return $this->render('commande/list.html.twig', [
-            'commandes' => $commandesDTO,
-            'totalPages'=>$nbrePages,
-            'currentPage'=>$page,
-            'formSearch' => $form->createView(),
-        ]);
-}
+        if (!$commande) {
+            throw $this->createNotFoundException('Commande introuvable');
+        }
+
+        // 🔹 Mise à jour de l’état
+        $commande->setStatut('Terminé');
+
+        $em->flush();
+
+        $this->addFlash('success', 'Commande marquée comme terminée.');
+
+        return $this->redirectToRoute('app_commande_list');
+    }
+
+    #[Route('/commande/{id}/annuler', name: 'app_commande_annuler', methods: ['GET'])]
+    public function annulerCommande(int $id,EntityManagerInterface $em): Response {
+        $commande = $em->getRepository(Commande::class)->find($id);
+
+        if (!$commande) {
+            throw $this->createNotFoundException('Commande introuvable');
+        }
+
+        $em->remove($commande);
+        $em->flush();
+
+        $this->addFlash('success', 'La commande a été annulée avec succès.');
+
+        return $this->redirectToRoute('app_commande_list');
+    }
+
+
 }
